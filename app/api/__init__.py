3cd64a272f2b73a442b2f3827c7c2c6f86c6f1e3
Kasen <18170166+imkasen@users.noreply.github.com>
2024-01-29 15:02:07 +0800

    move loguru config

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,21 +1,5 @@
 """
 API requests module
 """
-import os
-from pathlib import Path
-
-from loguru import logger
 
 from .ttsmaker import TTSMaker
-
-PROJ_ROOT: str = Path().resolve()
-
-logger.add(
-    # sink=os.path.join(PROJ_ROOT, "logs", "{time}.log"),
-    sink=os.path.join(PROJ_ROOT, "logs", "logger.log"),
-    format="{time:YYYY-MM-DD HH:mm::ss} | {level} | {message}",
-    encoding="utf-8",
-    enqueue=True,
-    rotation="00:00",
-    retention="1 week",
-)
--- app/entry.py
+++ app/entry.py
@@ -1,7 +1,21 @@
 """
 Project entry file
 """
+import os
+from pathlib import Path
+
+from loguru import logger
 from web import ui
+
+logger.add(
+    # sink=os.path.join(Path().resolve(), "logs", "{time}.log"),
+    sink=os.path.join(Path().resolve(), "logs", "logger.log"),
+    format="{time:YYYY-MM-DD HH:mm::ss} | {level} | {message}",
+    encoding="utf-8",
+    enqueue=True,
+    rotation="00:00",
+    retention="1 week",
+)
 
 if __name__ == "__main__":
     ui.queue().launch(
